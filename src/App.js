6762baf40578edc84efaf1c4f10a5a3e1d4a4aa2
Ashley Woodall <ash.woodall@gmail.com>
2019-07-18 22:18:33 -0500

    update logos and responsive breakpoints

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <About />
       {/*<Portfolio />*/}
+      <Footer />
     </div>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -13,52 +13,31 @@ const Wrapper = styled.div`
     text-align: left;
   }
 
-  @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) {
+  @media (min-width: 768px) {
     padding-top: 64px;
     justify-content: flex-start;
     margin: 0 32px;
-    font-size: 24px;
+    font-size: 32px;
 
     h1 {
       font-size: 64px;
     }
   }
-`;
 
-const ImgContainer = styled.div`
-  display: inline-block;
-  width: 48px;
-  margin-top: 48px;
+  @media (min-width: 1024px) {
+    max-width: 800px;
+    text-align: center;
+    margin: 0 auto;
+  }
 `;
 
 const About = () => (
   <Wrapper>
     <h1>Hi, I'm Ash</h1>
     <p>
-      Software engineer specializing in User Experience and Front-End
-      development while making all kinds of cool things in Austin, TX.{" "}
+      Software engineer and code educator specializing in User Experience and
+      Front-End development. Making all kinds of cool things in Austin, TX.
     </p>
-    <p>
-      Please look around, check out my{" "}
-      <a
-        href="https://github.com/ashwoodall"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Github
-      </a>
-      , my{" "}
-      <a
-        href="https://www.linkedin.com/in/ashwoodall/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        LinkedIn
-      </a>
-      , or feel free to <a href="#">contact me</a>. Don't hesitate to ask any
-      questions, y'all.
-    </p>
-    <ImgContainer />{" "}
   </Wrapper>
 );
 
